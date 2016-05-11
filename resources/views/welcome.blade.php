5a92f9b2c14cc649a5138c1e0a98acad1edd64a9
ianip6 <ianip6@yahoo.com>
2016-05-11 07:02:29 -0400

    Message Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 is HERE</div>
             </div>
         </div>
     </body>
